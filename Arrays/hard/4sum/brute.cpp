6f36151ed093cbd8b0092b91ba1160cfb6ab9d2c
GADDE NAGA DEEPIKA <nagadeepika943@gmail.com>
2026-06-28 11:10:11 +0530

    4sum brute

---
+++ Arrays/hard/4sum/brute.cpp
@@ -0,0 +1,31 @@
+// LeetCode 1. 4 Sum
+// Topic: Arrays
+// Difficulty: Hard
+class Solution {
+    public:
+        vector<vector<int>> fourSum(vector<int>& nums, int target) {
+            int n = nums.size();
+            set<vector<int>> st;
+    
+            for (int i = 0; i < n; i++) {
+                for (int j = i + 1; j < n; j++) {
+                    for (int k = j + 1; k < n; k++) {
+                        for (int l = k + 1; l < n; l++) {
+    
+                            long long sum = (long long)nums[i] +nums[j]+nums[k]+nums[l];
+                            if (sum == target) {
+                                vector<int> temp = {
+                                    nums[i], nums[j], nums[k], nums[l]
+                                };
+                                sort(temp.begin(), temp.end());
+                                st.insert(temp);
+                            }
+                        }
+                    }
+                }
+            }
+    
+            vector<vector<int>> ans(st.begin(), st.end());
+            return ans;
+        }
+    };
