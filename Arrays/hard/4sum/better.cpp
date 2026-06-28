f443de8f0a387b245c1d7f67a3b31677747eb5a7
GADDE NAGA DEEPIKA <nagadeepika943@gmail.com>
2026-06-28 16:31:48 +0530

    4sum better

--- Arrays/hard/4sum/better.cpp
+++ Arrays/hard/4sum/better.cpp
@@ -1,27 +1,35 @@
 // LeetCode 1. 4 Sum
 // Topic: Arrays
 // Difficulty: Hard
-lass Solution {
+class Solution {
     public:
         vector<vector<int>> fourSum(vector<int>& nums, int target) {
+            int n = nums.size();
             set<vector<int>> st;
-            int n=nums.size();
-            for(int i=0;i<n;i++){
-                for(int j=i+1;j<n;j++){
-                    set<long long>mpp;
-                    for(int k=j+1;k<n;k++){
-                       long long sum=nums[i]+nums[j]+nums[k];
-                       long long forth=target-sum;
-                       if(mpp.find(forth)!=mpp.end()){
-                        vector<int>temp={nums[i],nums[j],nums[k],(int)forth};
-                        sort(temp.begin(),temp.end());
-                        st.insert(temp);
-                       }
-                       mpp.insert(nums[k]);                    
+            for (int i = 0; i < n; i++) {
+                for (int j = i + 1; j < n; j++) {
+                    set<long long> mpp;
+                    for (int k = j + 1; k < n; k++) {
+                        long long sum =
+                            (long long)nums[i] + nums[j] + nums[k];
+                        long long forth = (long long)target - sum;
+                        if (mpp.find(forth) != mpp.end()) {
+    
+                            vector<int> temp = {
+                                nums[i],
+                                nums[j],
+                                nums[k],
+                                (int)forth
+                            };
+    
+                            sort(temp.begin(), temp.end());
+                            st.insert(temp);
+                        }
+                        mpp.insert(nums[k]);
                     }
                 }
             }
-            vector<vector<int>>v(st.begin(),st.end());
-            return v;
+            vector<vector<int>> ans(st.begin(), st.end());
+            return ans;
         }
     };
