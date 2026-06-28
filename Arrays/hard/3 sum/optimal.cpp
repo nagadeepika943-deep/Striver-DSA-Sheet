5c8632f5ce3702594c5249de768ae061f47635e5
GADDE NAGA DEEPIKA <nagadeepika943@gmail.com>
2026-06-28 10:06:20 +0530

    3 sum optimal

---
+++ Arrays/hard/3 sum/optimal.cpp
@@ -0,0 +1,39 @@
+// LeetCode 3. 3 Sum
+// Topic: Arrays
+// Difficulty: Hard
+class Solution {
+    public:
+        vector<vector<int>> threeSum(vector<int>& nums) {
+            int n=nums.size();
+            vector<vector<int>>v;
+            sort(nums.begin(),nums.end());
+            for(int i=0;i<n;i++){
+                if(i > 0 && nums[i] == nums[i-1]) continue;
+                int j=i+1;
+                int k=n-1;
+                while(j<k){
+                    int sum=nums[i]+nums[j]+nums[k];
+                    if(sum<0){
+                        j++;
+                    }
+                    else if(sum>0){
+                        k--;
+                    }
+                    else{
+                        vector<int>temp={nums[i],nums[j],nums[k]};
+                        v.push_back(temp);
+                        j++;
+                        k--;
+                        while(j<k && nums[j]==nums[j-1]){
+                            j++;
+                        }
+                        while(j<k && nums[k+1]==nums[k]){
+                            k--;
+                        }
+                    }
+                }
+    
+            }
+            return v;
+        }
+    };
