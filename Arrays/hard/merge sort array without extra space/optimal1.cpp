c10510d4876898d1d9f0800dfa35ddc514a3e535
GADDE NAGA DEEPIKA <nagadeepika943@gmail.com>
2026-06-30 14:27:27 +0530

    merge sort optimal1

---
+++ Arrays/hard/merge sort array without extra space/optimal1.cpp
@@ -0,0 +1,22 @@
+/ LeetCode :not in leetcode
+// Topic: Arrays
+// Difficulty: Hard
+class solution{
+    public:
+        void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+            int left=m-1;
+            int right=0;
+            while(left>=0 && right<n){
+                if(nums1[left]>nums2[right]){
+                    swap(nums1[left],nums2[right]);
+                    left--;
+                    right++;
+                }
+                else{
+                    break;
+                }
+            }
+            sort(nums1.begin(),nums1.end());
+            sort(nums2.begin(),nums2.end());
+        }
+    };
